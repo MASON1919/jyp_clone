2b67d2cbb88e8e38ff56a29132eb63ed91bdd48e
suno <sunoc303@gmail.com>
2025-09-04 17:24:25 +0900

    num_1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 // 최선호, 진창훈, 이은광
+document.addEventListener('DOMContentLoaded', () => {
+    const navbarOriginal = document.getElementById('navbar-original');
+    const hamburgerBtn = document.getElementById('hamburger-btn');
+    const hamburgerBtn2 = document.getElementById('hamburger-btn2');
+    const fullScreenMenu = document.getElementById('full-screen-menu');
+    const fullScreenBackground = document.getElementById('full-screen-background');
+    /*const logo = document.getElementById('logo');
+    const container = document.getElementById('container');
+    const header = document.getElementById('header');*/
+
+    hamburgerBtn.addEventListener('click', () => {
+        fullScreenMenu.classList.toggle('active');
+        fullScreenBackground.classList.toggle('active');
+        navbarOriginal.classList.toggle('display-none');
+    });
+    hamburgerBtn2.addEventListener('click', () => {
+        fullScreenMenu.classList.toggle('active');
+        fullScreenBackground.classList.toggle('active');
+        navbarOriginal.classList.toggle('display-none');
+    });
+});
