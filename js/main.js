57a6bbd5fd013c7386379cd4700ddb0326471b83
진창훈 <chj6966@gamil.com>
2025-09-05 10:46:11 +0900

    2번쨰커밋

--- js/main.js
+++ js/main.js
@@ -1,2 +1,50 @@
 // 최선호, 진창훈, 이은광
 
+<<<<<<< HEAD
+=======
+//----- 진창훈-//
+const slides = document.querySelectorAll('.slides img');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+let index = 0;
+let autoSlide;
+
+function showSlide(i) {
+  slides.forEach((slide, idx) => {
+    slide.classList.toggle('active', idx === i);
+  });
+}
+
+function nextSlide() {
+  index = (index + 1) % slides.length;
+  showSlide(index);
+}
+
+function prevSlide() {
+  index = (index - 1 + slides.length) % slides.length;
+  showSlide(index);
+}
+
+nextBtn.addEventListener('click', () => {
+  nextSlide();
+  resetAutoSlide();
+});
+
+prevBtn.addEventListener('click', () => {
+  prevSlide();
+  resetAutoSlide();
+});
+
+function startAutoSlide() {
+  autoSlide = setInterval(nextSlide, 5000);
+}
+
+function resetAutoSlide() {
+  clearInterval(autoSlide);
+  startAutoSlide();
+}
+
+// 초기 실행
+showSlide(index);
+startAutoSlide();
+>>>>>>> 4d9683b (N0.2)
